bebcc5423400720d3a7423fd5dec6c681f9b04e4
Mikko Impiö <mikko.impio@gmail.com>
2020-04-23 19:50:52 +0300

    security fixes

--- booksite/settings.py
+++ booksite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^pz=dgv3orn)sb1vh=&z*b$lt$1-z#st)r9m=yn@t+14(sv7^^'
+with open('booksite/.env') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
