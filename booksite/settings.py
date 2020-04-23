1693a7ce65f736f85d3a1857ce03fdc17bc3b0aa
Mikko Impiö <mikko.impio@gmail.com>
2020-04-23 21:19:17 +0300

    change key file

--- booksite/settings.py
+++ booksite/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('booksite/.env') as f:
-    SECRET_KEY = f.read().strip()
+from .keys import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
