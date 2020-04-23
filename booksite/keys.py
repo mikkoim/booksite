a9b48b978d2851f7f5c4358c070f1fe049b94e85
Mikko Impiö <mikko.impio@gmail.com>
2020-04-23 21:12:15 +0300

    fix keys

---
+++ booksite/keys.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '4p_a&2m*47kup^!4w^tw#th@r5o=34&_3vb17_wmr%jz1wov+#' 
--- booksite/settings.py
+++ booksite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('booksite/.env') as f:
-    SECRET_KEY = f.read().strip()
+from .keys import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
